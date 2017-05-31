b07f13fd03902c3b43c7583cbe493bfdd602e65d
Jin Ku <jin@sendbird.com>
2017-05-31 13:59:07 -0700

    add missing .eslintrc.js

---
+++ web-live-chat/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es6': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'sourceType': 'module'
+  },
+  'parser': 'babel-eslint',
+  'rules': {
+    'indent': ['error', 2],
+    'semi': 1,
+    'no-console': 1,
+    'camelcase': 1,
+    'no-unused-vars': 1
+  }
+};

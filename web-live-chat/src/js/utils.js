ffe737158a92d815220192fe949301a0801bb82e
James Kim <alchemist85k@gmail.com>
2017-07-25 14:54:41 +0900

    xssEscape function bugfix

--- web-live-chat/src/js/utils.js
+++ web-live-chat/src/js/utils.js
@@ -63,16 +63,20 @@ export function getPaddingTop(target) {
 }
 
 export function xssEscape(target) {
-  return target
-    .split('&').join('&amp;')
-    .split('#').join('&#35;')
-    .split('<').join('&lt;')
-    .split('>').join('&gt;')
-    .split('"').join('&quot;')
-    .split('\'').join('&apos;')
-    .split('+').join('&#43;')
-    .split('-').join('&#45;')
-    .split('(').join('&#40;')
-    .split(')').join('&#41;')
-    .split('%').join('&#37;');
+  if (typeof target === 'string') {
+    return target
+      .split('&').join('&amp;')
+      .split('#').join('&#35;')
+      .split('<').join('&lt;')
+      .split('>').join('&gt;')
+      .split('"').join('&quot;')
+      .split('\'').join('&apos;')
+      .split('+').join('&#43;')
+      .split('-').join('&#45;')
+      .split('(').join('&#40;')
+      .split(')').join('&#41;')
+      .split('%').join('&#37;');
+  } else {
+    return target;
+  }
 }

0fe1a928e4236c05a328cc4d5a1959b8179eb980
I Putu Yoga Permana <putuyoga@gmail.com>
2018-08-18 17:12:17 +0700

    fix overlapped keyboard on text input

--- react-native-redux-sample/ReactNativeWithSendBird/src/screens/Login.js
+++ react-native-redux-sample/ReactNativeWithSendBird/src/screens/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, Image, AsyncStorage } from 'react-native';
+import { View, Text, TextInput, KeyboardAvoidingView, Image, ScrollView, AsyncStorage } from 'react-native';
 import { connect } from 'react-redux';
 import { initLogin, sendbirdLogin } from '../actions'
 import {
@@ -69,60 +69,66 @@ class Login extends Component {
 
     render() {
         return (
-            <View style={styles.containerStyle}>
-                <Spinner visible={this.state.isLoading} />
-                <View style={styles.logoViewStyle}>
-                    <Image 
-                        style={{width: 150, height: 150}}
-                        source={require('../img/icon_sb_512.png')}
-                    />
-                    <Text style={styles.logoTextTitle}>SendBird</Text>
-                    <Text style={styles.logoTextSubTitle}>React Native</Text>
-                </View>
+            <ScrollView style={styles.containerStyle}>
+                <KeyboardAvoidingView
+                    style={styles.containerStyle}
+                    behavior={'position'}
+                    enabled
+                >
+                    <Spinner visible={this.state.isLoading} />
+                    <View style={styles.logoViewStyle}>
+                        <Image 
+                            style={{width: 150, height: 150}}
+                            source={require('../img/icon_sb_512.png')}
+                        />
+                        <Text style={styles.logoTextTitle}>SendBird</Text>
+                        <Text style={styles.logoTextSubTitle}>React Native</Text>
+                    </View>
 
-                <View style={styles.inputViewStyle}>
-                    <TextInput 
-                        label='User ID'
-                        placeholder='User ID'
-                        style={styles.inputStyle}
-                        value={this.state.userId}
-                        duration={100}
-                        autoCorrect={false}
-                        maxLength={16}
-                        underlineColorAndroid='transparent'
-                        onChangeText={this._onUserIdChanged}
-                    />
-                </View>
+                    <View style={styles.inputViewStyle}>
+                        <TextInput 
+                            label='User ID'
+                            placeholder='User ID'
+                            style={styles.inputStyle}
+                            value={this.state.userId}
+                            duration={100}
+                            autoCorrect={false}
+                            maxLength={16}
+                            underlineColorAndroid='transparent'
+                            onChangeText={this._onUserIdChanged}
+                        />
+                    </View>
 
-                <View style={styles.inputViewStyle}>
-                    <TextInput 
-                        label='Nickname'
-                        placeholder='Nickname'
-                        style={styles.inputStyle}
-                        value={this.state.nickname}
-                        duration={100}
-                        autoCorrect={false}
-                        maxLength={16}
-                        underlineColorAndroid='transparent'
-                        onChangeText={this._onNicknameChanged}
-                    />
-                </View>
+                    <View style={styles.inputViewStyle}>
+                        <TextInput 
+                            label='Nickname'
+                            placeholder='Nickname'
+                            style={styles.inputStyle}
+                            value={this.state.nickname}
+                            duration={100}
+                            autoCorrect={false}
+                            maxLength={16}
+                            underlineColorAndroid='transparent'
+                            onChangeText={this._onNicknameChanged}
+                        />
+                    </View>
 
-                <View style={styles.buttonStyle}>
-                    <Button
-                        title='CONNECT'
-                        buttonStyle={{backgroundColor: '#6e5baa'}}
-                        onPress={this._onButtonPress}
-                        disabled={this.state.isLoading}
-                    />
-                </View>
-                
-                <Text style={styles.errorTextStyle}>{this.props.error}</Text>
+                    <View style={styles.buttonStyle}>
+                        <Button
+                            title='CONNECT'
+                            buttonStyle={{backgroundColor: '#6e5baa'}}
+                            onPress={this._onButtonPress}
+                            disabled={this.state.isLoading}
+                        />
+                    </View>
+                    
+                    <Text style={styles.errorTextStyle}>{this.props.error}</Text>
 
-                <View style={[styles.footerViewStyle]}>
-                    <Text style={styles.footerTextStyle}>Sample UI v2.1.2 / SDK v.3.0.61</Text>
-                </View>
-            </View>
+                    <View style={[styles.footerViewStyle]}>
+                        <Text style={styles.footerTextStyle}>Sample UI v2.1.2 / SDK v.3.0.61</Text>
+                    </View>
+                </KeyboardAvoidingView>
+            </ScrollView>
         );
     }
 }

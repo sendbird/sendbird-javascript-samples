4b4d75cb4347602a8a0d711ac5a0625d2cfb4164
Sravan S <sravan@sendbird.com>
2020-03-29 16:47:39 +0900

    Init Chat component

--- uikit-samples/custom-react-app/src/App.js
+++ uikit-samples/custom-react-app/src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import Login from './Login';
+import Chat from './Chat/Index';
+
 function App() {
+  const [config, setconfig] = useState({});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <Chat
+              userId={config.userId}
+              nickname={config.nickname}
+              theme={config.theme}
+            />
+            Chat
+          </Route>
+          <Route path="/">
+            <Login onSubmit={setconfig} />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ uikit-samples/custom-react-app/src/Chat/Index.jsx
@@ -0,0 +1,59 @@
+import './index.css';
+
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import {
+  SendbirdProvider,
+  ChannelList,
+  Channel,
+  ChannelSettings,
+} from 'sendbird-uikit';
+import 'sendbird-uikit/dist/index.css';
+
+export default function Chat({ userId, theme, nickname }) {
+  const history = useHistory();
+  useEffect(() => {
+    if (!userId || !nickname) {
+      history.push('/');
+    }
+  }, [userId, nickname, history]);
+  const [showSettings, setShowSettings] = useState(false);
+  const [currentChannelUrl, setCurrentChannelUrl] = useState(null);
+
+  return (
+    <div style={{ height: '100vh' }}>
+      <SendbirdProvider
+        appId={process.env.APP_ID}
+        theme={theme}
+        userId={userId}
+        nickname={nickname}
+      >
+        <div className="sendbird-app__wrap">
+          <div className="sendbird-app__channellist-wrap">
+            <ChannelList
+              onChannelSelect={(channel) => {
+                if (channel && channel.url) {
+                  setCurrentChannelUrl(channel.url);
+                }
+              }}
+            />
+          </div>
+          <div className="sendbird-app__conversation-wrap">
+            <Channel
+              channelUrl={currentChannelUrl}
+              onChatHeaderActionClick={() => { setShowSettings(true); }}
+            />
+          </div>
+        </div>
+        {showSettings && (
+          <div className="sendbird-app__settingspanel-wrap">
+            <ChannelSettings
+              channelUrl={currentChannelUrl}
+              onCloseClick={() => { setShowSettings(false); }}
+            />
+          </div>
+        )}
+      </SendbirdProvider>
+    </div>
+  )
+}

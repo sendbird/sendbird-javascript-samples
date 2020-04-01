c692bfce180b16605ac174e810a38f8c51587d24
Sravan S <sravan@sendbird.com>
2020-04-01 12:40:51 +0900

    Add read me

--- uikit-samples/simple-react-app/src/App.js
+++ uikit-samples/simple-react-app/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { App as SendbirdApp } from 'sendbird-uikit';
+import { App as SendBirdApp } from 'sendbird-uikit';
 import 'sendbird-uikit/dist/index.css';
 
 function App() {
   return (
     <div className="App">
-      <SendbirdApp
+      <SendBirdApp
         appId={process.env.APP_ID}
         userId="sendbird"
         nickname="sendbird"

b68c38bbc80c88cbc9cd052c47d2034ebf53bbeb
Denis Gao <denisgao@buffalo.edu>
2021-10-28 21:07:48 -0400

    First Commit

--- web-basic-sample/src/js/const.js
+++ web-basic-sample/src/js/const.js
@@ -1,4 +1,4 @@
-export const APP_ID = '9DA1B1F4-0BE6-4DA8-82C5-2E81DAB56F23';
+export const APP_ID = 'C9C493CF-0BD3-472F-9E24-F1D0B31FAC78';
 export const USER_ID = 'user_id';
 export const DISPLAY_NONE = 'none';
 export const DISPLAY_BLOCK = 'block';

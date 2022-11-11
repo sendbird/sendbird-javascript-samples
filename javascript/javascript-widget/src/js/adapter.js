282238d1c770d3e617e0c14b9a99f65603f39916
danney <danney.chun@sendbird.com>
2022-11-11 10:26:45 +0900

    added security comment

--- javascript/javascript-widget/src/js/adapter.js
+++ javascript/javascript-widget/src/js/adapter.js
@@ -218,9 +218,14 @@ class SendBirdAdapter {
         resolve(prevUsers);
       });
   }
-
-  /*
-  User
+  /**
+   * 
+   * #################### SECURITY TIPS ####################
+   * Before launching, you should review "Allow retrieving user list from SDK" under ⚙️ Sendbird Dashboard ->Settings -> Security.
+   * It's turned on at first to simplify running samples and implementing your first code.
+   * Most apps will want to disable "Allow retrieving user list from SDK" as that could possibly expose user information
+   * #################### SECURITY TIPS ####################
+   * 
    */
   getUserList(channelUrl, cb) {
     if(!this.userListQuery) {
